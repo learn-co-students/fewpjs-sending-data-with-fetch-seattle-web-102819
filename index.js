965e35f50b74b9beb03d69d7fb4f61440268b0c0
moliver4 <tifftang22@gmail.com>
2019-12-12 14:00:52 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function addData(data){
+    document.body.innerHTML = data[ "id" ]
+}
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name: name,
+          email: email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( data ) {
+        addData(data)
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+
