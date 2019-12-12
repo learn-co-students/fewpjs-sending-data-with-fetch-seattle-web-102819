e0bc555cbafa75243fed529f3f468cfb1b82778f
devincloudkelly <devin.cloud.kelly@gmail.com>
2019-12-12 14:24:38 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+// fetch('http://localhost:3000/dogs', {
+//     method: "POST"
+//     headers: {
+//         "Content-Type": "application/json"
+//         Accept: "application/json"
+//     }
+//     body: JSON.stringify(dog)
+// })
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("something isn't working");
+//         console.log(error.message);
+//     });
+
+    function submitData(name, email) {
+        return fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email
+            })
+        })
+        
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            makeParagraph(object.id);
+        })
+        .catch(function(error) {
+            alert("Unauthorized Access");
+            makeParagraph(error.message);
+        })
+    }
+
+    function makeParagraph(input) {
+        const body = document.body
+        const paragraph = document.createElement('p')
+        paragraph.textContent = input
+        body.appendChild(paragraph)
+    }
